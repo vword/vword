2fa091b253f006fd56714479ae7c34b1fb2be9ac
chun mun <chunmuntan@yahoo.com>
2016-07-08 18:14:12 +0800

    initial draft

---
+++ fuel/application/libraries/MY_Input.php
@@ -0,0 +1,15 @@
+<?php
+
+class MY_Input extends CI_Input {
+
+        function __construct()
+        {
+            parent::__construct();
+        }
+
+       function load_query(){
+      //here what the method did
+      }
+    }
+
+?>
---
+++ fuel/application/views/_blocks/test_footer.php
@@ -0,0 +1,16 @@
+
+			</div>
+	</div>
+	<div class="wrapper">
+	<footer class="row footer">
+            <p class="colophon"><center>vword copyright</center></p> 
+	</footer>
+</div>
+
+
+<?php 
+     echo js(fuel_var('js'));
+?>
+
+</body>
+</html>
