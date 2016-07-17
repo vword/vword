f0b17e2f9e54b9c65ef9e9982065fad05ae1694b
user <user@192.168.182.1>
2016-07-17 21:15:59 +0800

    change edit always route to fuel

--- edit/fuel/application/controllers/Welcome.php
+++ edit/fuel/application/controllers/Welcome.php
@@ -22,6 +22,8 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->helper('url');
 
-		$this->load->view('welcome_message');
+		//$this->load->view('welcome_message');
+                header('Location: index.php/fuel/login');
+                die();
 	}
 }
